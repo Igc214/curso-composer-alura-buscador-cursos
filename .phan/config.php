83674ccd9603f0c676a18b87418c260a9011c376
Igor Carvalho <igor.quantum21@gmail.com>
2024-04-10 14:52:04 -0300

    Primeiro commit

---
+++ .phan/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    "target_php_version" => '8.3.2',
+    'directory_list' => [
+        'src',
+        'vendor/symfony/dom-crawler',
+        'vendor/guzzlehttp/guzzle',
+        'vendor/psr/http-message'
+    ],
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+    'plugins' => [
+        'AlwaysReturnPlugin',
+        'DollarDollarPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'PregRegexCheckerPlugin',
+        'PrintfCheckerPlugin',
+    ],
+];
